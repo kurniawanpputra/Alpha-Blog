ade7b907e6a821919614feaf4f7c4e1b0983d823
Kurniawan Pratama <kurniawanpputra@gmail.com>
2018-08-27 12:52:34 +0000

    Create Show, Flash Messages, Create Method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,4 @@
 class ArticlesController < ApplicationController
-  def index
-    
-  end
-  
   def new
     @article = Article.new
   end
@@ -10,8 +6,16 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article saved"
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+  
+  def show
+    @article = Article.find(params[:id])
   end
   
   private
